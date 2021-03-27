c4fc272f34d12d51d8bb112e5a8b826a05cb8204
Weiwei Wan <wanweiwei07@gmail.com>
2021-03-27 18:44:17 +0900

    sda5f

--- robotsim/manipulators/sia5/sia5.py
+++ robotsim/manipulators/sia5/sia5.py
@@ -6,17 +6,17 @@ import robotsim._kinematics.jlchain as jl
 import robotsim.manipulators.manipulator_interface as mi
 
 
-class SIA5F(mi.ManipulatorInterface):
+class SIA5(mi.ManipulatorInterface):
 
-    def __init__(self, pos=np.zeros(3), rotmat=np.eye(3), homeconf=np.zeros(7), name='sia5f', enable_cc=True):
+    def __init__(self, pos=np.zeros(3), rotmat=np.eye(3), homeconf=np.zeros(7), name='sia5', enable_cc=True):
         super().__init__(pos=pos, rotmat=rotmat, name=name)
         this_dir, this_filename = os.path.split(__file__)
         self.jlc = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=homeconf, name=name)
         # seven joints, njnts = 7+2 (tgt ranges from 1-7), nlinks = 7+1
         jnt_safemargin = math.pi / 18.0
-        self.jlc.jnts[1]['loc_pos'] = np.array([0, 0, .31])
+        self.jlc.jnts[1]['loc_pos'] = np.array([0, 0, 0])
         self.jlc.jnts[1]['motion_rng'] = [-math.pi + jnt_safemargin, math.pi]
-        self.jlc.jnts[2]['loc_pos'] = np.array([0, 0, 0])
+        self.jlc.jnts[2]['loc_pos'] = np.array([0, 0, .168])
         self.jlc.jnts[2]['motion_rng'] = [-math.radians(110) + jnt_safemargin, math.radians(110) - jnt_safemargin]
         self.jlc.jnts[2]['loc_motionax'] = np.array([0, 1, 0])
         self.jlc.jnts[3]['loc_pos'] = np.array([0, 0, .27])
@@ -31,28 +31,30 @@ class SIA5F(mi.ManipulatorInterface):
         self.jlc.jnts[6]['motion_rng'] = [-math.radians(110) + jnt_safemargin, math.radians(110) - jnt_safemargin]
         self.jlc.jnts[6]['loc_motionax'] = np.array([0, -1, 0])
         self.jlc.jnts[7]['loc_pos'] = np.array([.134, .0, .0])
-        self.jlc.jnts[7]['loc_rotmat'] = rm.rotmat_from_euler(-1.5708, 0, 0)
+        self.jlc.jnts[7]['loc_rotmat'] = rm.rotmat_from_euler(-math.pi/2, 0, 0)
         self.jlc.jnts[7]['motion_rng'] = [-math.radians(90) + jnt_safemargin, math.radians(90) - jnt_safemargin]
         self.jlc.jnts[7]['loc_motionax'] = np.array([-1, 0, 0])
         self.jlc.jnts[8]['loc_pos'] = np.array([.011, .0, .0])
-        self.jlc.jnts[8]['loc_rotmat'] = rm.rotmat_from_euler(-1.5708, -1.5708, -1.5708)
+        self.jlc.jnts[8]['loc_rotmat'] = rm.rotmat_from_euler(-math.pi/2, -math.pi/2, -math.pi/2)
         # links
-        self.jlc.lnks[0]['name'] = "base"
-        self.jlc.lnks[0]['loc_pos'] = np.zeros(3)
-        self.jlc.lnks[0]['meshfile'] = os.path.join(this_dir, "meshes", "base.dae")
-        self.jlc.lnks[0]['rgba'] = [.5,.5,.5, 1.0]
+        # self.jlc.lnks[0]['name'] = "base"
+        # self.jlc.lnks[0]['loc_pos'] = np.zeros(3)
+        # self.jlc.lnks[0]['meshfile'] = os.path.join(this_dir, "meshes", "base.dae")
+        # self.jlc.lnks[0]['rgba'] = [.5,.5,.5, 1.0]
         self.jlc.lnks[1]['name'] = "link_s"
-        self.jlc.lnks[1]['loc_pos'] = np.zeros(3)
+        self.jlc.lnks[1]['loc_pos'] = np.array([0, 0, .142])
+        self.jlc.lnks[1]['loc_rotmat'] = rm.rotmat_from_euler(0, 0, math.pi)
         self.jlc.lnks[1]['meshfile'] = os.path.join(this_dir, "meshes", "link_s.dae")
-        self.jlc.lnks[1]['rgba'] = [.35,.35,.35, 1.0]
+        self.jlc.lnks[1]['rgba'] = [.55,.55,.55, 1.0]
         self.jlc.lnks[2]['name'] = "link_l"
         self.jlc.lnks[2]['loc_pos'] = np.zeros(3)
+        self.jlc.lnks[2]['loc_rotmat'] = rm.rotmat_from_euler(0, 0, math.pi)
         self.jlc.lnks[2]['meshfile'] = os.path.join(this_dir, "meshes", "link_l.dae")
         self.jlc.lnks[2]['rgba'] = [.1,.3,.5, 1.0]
         self.jlc.lnks[3]['name'] = "link_e"
         self.jlc.lnks[3]['loc_pos'] = np.zeros(3)
         self.jlc.lnks[3]['meshfile'] = os.path.join(this_dir, "meshes", "link_e.dae")
-        self.jlc.lnks[3]['rgba'] = [.35,.35,.35, 1.0]
+        self.jlc.lnks[3]['rgba'] = [.7,.7,.7, 1.0]
         self.jlc.lnks[4]['name'] = "link_u"
         self.jlc.lnks[4]['loc_pos'] = np.zeros(3)
         self.jlc.lnks[4]['meshfile'] = os.path.join(this_dir, "meshes", "link_u.dae")
@@ -64,7 +66,7 @@ class SIA5F(mi.ManipulatorInterface):
         self.jlc.lnks[6]['name'] = "link_b"
         self.jlc.lnks[6]['loc_pos'] = np.zeros(3)
         self.jlc.lnks[6]['meshfile'] = os.path.join(this_dir, "meshes", "link_b.dae")
-        self.jlc.lnks[6]['rgba'] = [.5,.5,.5, 1.0]
+        self.jlc.lnks[6]['rgba'] = [.1,.3,.5, 1.0]
         self.jlc.lnks[7]['name'] = "link_t"
         self.jlc.lnks[7]['loc_pos'] = np.zeros(3)
         self.jlc.lnks[7]['meshfile'] = os.path.join(this_dir, "meshes", "link_t.dae")
@@ -79,9 +81,8 @@ class SIA5F(mi.ManipulatorInterface):
 
     def enable_cc(self):
         super().enable_cc()
-        self.cc.add_cdlnks(self.jlc, [0, 1, 2, 3, 4, 5, 6, 7])
-        activelist = [self.jlc.lnks[0],
-                      self.jlc.lnks[1],
+        self.cc.add_cdlnks(self.jlc, [1, 2, 3, 4, 5, 6, 7])
+        activelist = [self.jlc.lnks[1],
                       self.jlc.lnks[2],
                       self.jlc.lnks[3],
                       self.jlc.lnks[4],
@@ -89,8 +90,7 @@ class SIA5F(mi.ManipulatorInterface):
                       self.jlc.lnks[6],
                       self.jlc.lnks[7]]
         self.cc.set_active_cdlnks(activelist)
-        fromlist = [self.jlc.lnks[0],
-                    self.jlc.lnks[1],
+        fromlist = [self.jlc.lnks[1],
                     self.jlc.lnks[2]]
         intolist = [self.jlc.lnks[5],
                     self.jlc.lnks[6],
@@ -105,7 +105,7 @@ if __name__ == '__main__':
 
     base = wd.World(campos=[2, 0, 1], lookatpos=[0, 0, 0.5])
     gm.gen_frame().attach_to(base)
-    manipulator_instance = SIA5F(enable_cc=True)
+    manipulator_instance = SIA5(enable_cc=True)
     manipulator_meshmodel = manipulator_instance.gen_meshmodel()
     manipulator_meshmodel.attach_to(base)
     manipulator_meshmodel.show_cdprimit()
--- robotsim/robots/sda5f/sda5f.py
+++ robotsim/robots/sda5f/sda5f.py
@@ -5,63 +5,74 @@ import basis.robot_math as rm
 import modeling.modelcollection as mc
 import modeling.collisionmodel as cm
 import robotsim._kinematics.jlchain as jl
-import robotsim.manipulators.ur3.ur3 as ur
+import robotsim.manipulators.sia5.sia5 as sia
 import robotsim.grippers.robotiq85.robotiq85 as rtq
 from panda3d.core import CollisionNode, CollisionBox, Point3
 import robotsim.robots.robot_interface as ri
 
 
-class UR3Dual(ri.RobotInterface):
+class SDA5F(ri.RobotInterface):
 
     def __init__(self, pos=np.zeros(3), rotmat=np.eye(3), name='ur3dual', enable_cc=True):
         super().__init__(pos=pos, rotmat=rotmat, name=name)
         this_dir, this_filename = os.path.split(__file__)
         # left side
-        self.lft_base = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=np.zeros(0), name='lft_base_jl')
-        self.lft_base.jnts[1]['loc_pos'] = np.array([.0, .258485281374, 1.61051471863])  # left from robot view
-        self.lft_base.jnts[1]['loc_rotmat'] = rm.rotmat_from_euler(-3.0 * math.pi / 4.0, 0, math.pi, 'rxyz')  # left from robot view
-        self.lft_base.lnks[0]['name'] = "ur3_dual_lft_base"
+        self.lft_base = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=np.zeros(1), name='lft_base_jl')
+        self.lft_base.jnts[1]['loc_pos'] = np.array([0.045, 0, 0.7296])
+        self.lft_base.jnts[2]['loc_pos'] = np.array([0.15, 0.101, 0.1704])
+        self.lft_base.jnts[2]['loc_rotmat'] = rm.rotmat_from_euler(-math.pi / 2.0, -math.pi / 2.0, 0)
+        self.lft_base.lnks[0]['name'] = "sda5f_lft_base"
         self.lft_base.lnks[0]['loc_pos'] = np.array([0, 0, 0])
         self.lft_base.lnks[0]['collisionmodel'] = cm.CollisionModel(
-            os.path.join(this_dir, "meshes", "ur3_dual_base.stl"),
+            os.path.join(this_dir, "meshes", "base_link.stl"),
             cdprimit_type="user_defined", expand_radius=.005,
             userdefined_cdprimitive_fn=self._base_combined_cdnp)
-        self.lft_base.lnks[0]['rgba'] = [.3, .3, .3, 1.0]
+        self.lft_base.lnks[0]['rgba'] = [.7, .7, .7, 1.0]
+        self.lft_base.lnks[1]['name'] = "sda5f_lft_torso"
+        self.lft_base.lnks[1]['loc_pos'] = np.array([0, 0, 0])
+        self.lft_base.lnks[1]['collisionmodel'] = cm.CollisionModel(
+            os.path.join(this_dir, "meshes", "torso_link.stl"),
+            cdprimit_type="user_defined", expand_radius=.005,
+            userdefined_cdprimitive_fn=self._torso_combined_cdnp)
+        self.lft_base.lnks[1]['rgba'] = [.7, .7, .7, 1.0]
         self.lft_base.reinitialize()
-        lft_arm_homeconf = np.zeros(6)
-        lft_arm_homeconf[0] = math.pi / 3.0
-        lft_arm_homeconf[1] = -math.pi * 1.0 / 3.0
-        lft_arm_homeconf[2] = -math.pi * 2.0 / 3.0
-        lft_arm_homeconf[3] = math.pi
-        lft_arm_homeconf[4] = -math.pi / 2.0
-        self.lft_arm = ur.UR3(pos=self.lft_base.jnts[-1]['gl_posq'],
-                              rotmat=self.lft_base.jnts[-1]['gl_rotmatq'],
-                              homeconf=lft_arm_homeconf,
-                              enable_cc=False)
+        lft_arm_homeconf = np.zeros(7)
+        # lft_arm_homeconf[0] = math.pi / 3.0
+        # lft_arm_homeconf[1] = -math.pi * 1.0 / 3.0
+        # lft_arm_homeconf[2] = -math.pi * 2.0 / 3.0
+        # lft_arm_homeconf[3] = math.pi
+        # lft_arm_homeconf[4] = -math.pi / 2.0
+        self.lft_arm = sia.SIA5(pos=self.lft_base.jnts[-1]['gl_posq'],
+                                rotmat=self.lft_base.jnts[-1]['gl_rotmatq'],
+                                homeconf=lft_arm_homeconf,
+                                enable_cc=False)
         # lft hand offset (if needed)
         self.lft_hnd_offset = np.zeros(3)
         lft_hnd_pos, lft_hnd_rotmat = self.lft_arm.cvt_loc_intcp_to_gl(loc_pos=self.lft_hnd_offset)
         self.lft_hnd = rtq.Robotiq85(pos=lft_hnd_pos,
                                      rotmat=self.lft_arm.jnts[-1]['gl_rotmatq'],
                                      enable_cc=False)
-        # rigth side
-        self.rgt_base = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=np.zeros(0), name='rgt_base_jl')
-        self.rgt_base.jnts[1]['loc_pos'] = np.array([.0, -.258485281374, 1.61051471863])  # right from robot view
-        self.rgt_base.jnts[1]['loc_rotmat'] = rm.rotmat_from_euler(3.0 * math.pi / 4.0, .0, .0)  # left from robot view
-        self.rgt_base.lnks[0]['name'] = "ur3_dual_rgt_base"
+        # right side
+        self.rgt_base = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=np.zeros(1), name='rgt_base_jl')
+        self.rgt_base.jnts[1]['loc_pos'] = np.array([0.045, 0, 0.7296])  # right from robot view
+        self.rgt_base.jnts[2]['loc_pos'] = np.array([0.15, -0.101, 0.1704])
+        self.rgt_base.jnts[2]['loc_rotmat'] = rm.rotmat_from_euler(math.pi / 2.0, -math.pi / 2.0, 0)
+        self.rgt_base.lnks[0]['name'] = "sda5f_rgt_base"
         self.rgt_base.lnks[0]['loc_pos'] = np.array([0, 0, 0])
         self.rgt_base.lnks[0]['meshfile'] = None
-        self.rgt_base.lnks[0]['rgba'] = [.3, .3, .3, 1.0]
+        self.rgt_base.lnks[1]['name'] = "sda5f_rgt_torso"
+        self.rgt_base.lnks[1]['loc_pos'] = np.array([0, 0, 0])
+        self.rgt_base.lnks[1]['meshfile'] = None
         self.rgt_base.reinitialize()
-        rgt_arm_homeconf = np.zeros(6)
-        rgt_arm_homeconf[0] = -math.pi * 1.0 / 3.0
-        rgt_arm_homeconf[1] = -math.pi * 2.0 / 3.0
-        rgt_arm_homeconf[2] = math.pi * 2.0 / 3.0
-        rgt_arm_homeconf[4] = math.pi / 2.0
-        self.rgt_arm = ur.UR3(pos=self.rgt_base.jnts[-1]['gl_posq'],
-                              rotmat=self.rgt_base.jnts[-1]['gl_rotmatq'],
-                              homeconf=rgt_arm_homeconf,
-                              enable_cc=False)
+        rgt_arm_homeconf = np.zeros(7)
+        # rgt_arm_homeconf[0] = -math.pi * 1.0 / 3.0
+        # rgt_arm_homeconf[1] = -math.pi * 2.0 / 3.0
+        # rgt_arm_homeconf[2] = math.pi * 2.0 / 3.0
+        # rgt_arm_homeconf[4] = math.pi / 2.0
+        self.rgt_arm = sia.SIA5(pos=self.rgt_base.jnts[-1]['gl_posq'],
+                                rotmat=self.rgt_base.jnts[-1]['gl_rotmatq'],
+                                homeconf=rgt_arm_homeconf,
+                                enable_cc=False)
         # rgt hand offset (if needed)
         self.rgt_hnd_offset = np.zeros(3)
         rgt_hnd_pos, rgt_hnd_rotmat = self.rgt_arm.cvt_loc_intcp_to_gl(loc_pos=self.rgt_hnd_offset)
@@ -88,24 +99,20 @@ class UR3Dual(ri.RobotInterface):
     @staticmethod
     def _base_combined_cdnp(name, radius):
         collision_node = CollisionNode(name)
-        collision_primitive_c0 = CollisionBox(Point3(0.18, 0.0, 0.105),
-                                              x=.61 + radius, y=.41 + radius, z=.105 + radius)
+        collision_primitive_c0 = CollisionBox(Point3(.0, 0.0, 0.225),
+                                              x=.14 + radius, y=.14 + radius, z=.225 + radius)
         collision_node.addSolid(collision_primitive_c0)
-        collision_primitive_c1 = CollisionBox(Point3(0.0, 0.0, 0.4445),
-                                              x=.321 + radius, y=.321 + radius, z=.2345 + radius)
+        collision_primitive_c1 = CollisionBox(Point3(0.031, 0.0, 0.73),
+                                              x=.0855 + radius, y=.0855 + radius, z=.27 + radius)
         collision_node.addSolid(collision_primitive_c1)
-        collision_primitive_c2 = CollisionBox(Point3(0.0, 0.0, 0.8895),
-                                              x=.05 + radius, y=.05 + radius, z=.6795 + radius)
+        return collision_node
+
+    @staticmethod
+    def _torso_combined_cdnp(name, radius):
+        collision_node = CollisionNode(name)
+        collision_primitive_c2 = CollisionBox(Point3(0.195, 0.0, 0.1704),
+                                              x=.085 + radius, y=.101 + radius, z=.09 + radius)
         collision_node.addSolid(collision_primitive_c2)
-        collision_primitive_c3 = CollisionBox(Point3(0.0, 0.0, 1.619),
-                                              x=.1 + radius, y=.275 + radius, z=.05 + radius)
-        collision_node.addSolid(collision_primitive_c3)
-        collision_primitive_l0 = CollisionBox(Point3(0.0, 0.300, 1.669),
-                                              x=.1 + radius, y=.029 + radius, z=.021 + radius)
-        collision_node.addSolid(collision_primitive_l0)
-        collision_primitive_r0 = CollisionBox(Point3(0.0, -0.300, 1.669),
-                                              x=.1 + radius, y=.029 + radius, z=.021 + radius)
-        collision_node.addSolid(collision_primitive_r0)
         return collision_node
 
     def enable_cc(self):
@@ -178,10 +185,9 @@ if __name__ == '__main__':
 
     base = wd.World(campos=[3, 0, 3], lookatpos=[0, 0, 1])
     gm.gen_frame().attach_to(base)
-    u3d = UR3Dual()
-    # u3d.fk(.85)
-    u3d_meshmodel = u3d.gen_meshmodel()
-    u3d_meshmodel.attach_to(base)
-    u3d_meshmodel.show_cdprimit()
-    u3d.gen_stickmodel().attach_to(base)
+    sdarbt = SDA5F()
+    sdarbt_meshmodel = sdarbt.gen_meshmodel()
+    sdarbt_meshmodel.attach_to(base)
+    # sdarbt_meshmodel.show_cdprimit()
+    sdarbt.gen_stickmodel().attach_to(base)
     base.run()
