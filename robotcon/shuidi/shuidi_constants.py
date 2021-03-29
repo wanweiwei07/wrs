12af6614dc735d7930473c2c1ceea3600a67beaa
weiwei <wanweiwei07@gmail.com>
2021-03-29 23:54:29 +0900

    yunji shuidi robocon

---
+++ robotcon/shuidi/shuidi_constants.py
@@ -0,0 +1,11 @@
+class ShuidiConstants:
+    """
+    Default constants for shuidi
+    author: weiwei
+    date: 20210329
+    """
+
+    IP = '192.168.10.10' # default ip
+    PORTS = {"server": 5000}
+    BUFSIZE = 31001
+    BUFSIZE = 4096
