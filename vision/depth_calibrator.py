a40ad0272e506c9bacb1e4b138c05cce0923f992
Weiwei Wan <wanweiwei07@gmail.com>
2021-04-08 22:05:17 +0900

    vision-calibration

---
+++ vision/depth_calibrator.py
@@ -0,0 +1,20 @@
+class DepthCaliberator(object):
+
+    def __init__(self, robot_exe, sensor_client):
+        pass
+
+    def _find_ee_in_sensor(self, component_name, action_pos_list, action_rotmat_list, aruco_info, criteria_radius = None):
+        """
+
+        :param component_name:
+        :param action_pos_list:
+        :param action_rotmat_list:
+        :param aruco_info:
+        :param criteria_radius: rough radius used to determine if the newly estimated center is correct or not
+        :return:
+        author: weiwei
+        date: 20210408
+        """
+
+    def find_board_center_in_hand(self, component_name, parameters,):
+        pass
