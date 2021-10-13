d4e115b2f9bd5bd832d7c1c9da26dd92396efcaf
Weiwei Wan <wanweiwei07@gmail.com>
2021-10-13 21:30:23 +0900

    spell miss

--- 0000_examples/xarm_shuidi_remote_conrol.py
+++ 0000_examples/xarm_shuidi_remote_conrol.py
@@ -10,7 +10,7 @@ import robot_con.xarm_shuidi.xarm_shuidi_client as rbx
 base = wd.World(cam_pos=[3, 1, 1.5], lookat_pos=[0, 0, 0.7])
 rbt_s = rbs.XArm7YunjiMobile()
 rbt_x = rbx.XArmShuidiClient(host="10.2.0.203:18300")
-jnt_values = rbt_x.arm_get_jnt_vlaues()
+jnt_values = rbt_x.arm_get_jnt_values()
 jawwidth = rbt_x.arm_get_jawwidth()
 rbt_s.fk(jnt_values=jnt_values)
 rbt_s.jaw_to(jawwidth=jawwidth)
