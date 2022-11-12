60deb5c0256942d5fb3a550ee4299fe61429f797
Hao Chen <chenhaox@outlook.com>
2022-11-12 21:35:32 +0900

    XARM_LITE6 Version2

---
+++ robot_sim/end_effectors/gripper/lite6_wrs_gripper/lite6_wrs_gripper_v2.py
@@ -0,0 +1,272 @@
+"""
+The simulation for the WRS gripper designed by Wan sensei
+Author: Chen Hao (chen960216@gmail.com), 20220924, osaka
+Reference: The code is implemented referring to the 'robot_sim/end_effectors/yumi_gripper/'
+"""
+import os
+import math
+import numpy as np
+import modeling.model_collection as mc
+import modeling.collision_model as cm
+from panda3d.core import CollisionNode, CollisionBox, Point3
+import robot_sim._kinematics.jlchain as jl
+import basis.robot_math as rm
+import robot_sim.end_effectors.gripper.gripper_interface as gp
+
+
+class Lite6WRSGripper2(gp.GripperInterface):
+
+    def __init__(self, pos=np.zeros(3), rotmat=np.eye(3), cdmesh_type='convex_hull', name='lite6_wrs_gripper2',
+                 enable_cc=True):
+        super().__init__(pos=pos, rotmat=rotmat, cdmesh_type=cdmesh_type, name=name)
+        this_dir, this_filename = os.path.split(__file__)
+        cpl_end_pos = self.coupling.jnts[-1]['gl_posq']
+        cpl_end_rotmat = self.coupling.jnts[-1]['gl_rotmatq']
+        # gripper base
+        self.body = jl.JLChain(pos=cpl_end_pos, rotmat=cpl_end_rotmat, homeconf=np.zeros(1), name='base')
+        self.body.jnts[1]['loc_pos'] = np.array([0, 0, 0])
+        self.body.lnks[0]['name'] = "base"
+        self.body.lnks[0]['loc_pos'] = np.zeros(3)
+        self.body.lnks[0]['collision_model'] = cm.CollisionModel(os.path.join(this_dir, "meshes", "base_v2.stl"),
+                                                                 expand_radius=.001)
+        self.body.lnks[0]['rgba'] = [.77, .77, .77, 1]
+
+        self.body.lnks[1]['name'] = "realsense_dual"
+        self.body.lnks[1]['loc_pos'] = np.zeros(3)
+        self.body.lnks[1]['collision_model'] = cm.CollisionModel(os.path.join(this_dir, "meshes", "dual_realsense.stl"),
+                                                                 expand_radius=.001)
+        self.body.lnks[1]['rgba'] = [.88, .88, .88, 1]
+
+        # lft finger
+        self.lft = jl.JLChain(pos=cpl_end_pos, rotmat=cpl_end_rotmat, homeconf=np.zeros(1), name='lft_finger')
+        self.lft.jnts[1]['loc_pos'] = np.array([0.01492498, 0.005, .05])
+        self.lft.jnts[1]['loc_rotmat'] = rm.rotmat_from_euler(0, 0, -math.pi)
+        self.lft.jnts[1]['type'] = 'prismatic'
+        self.lft.jnts[1]['motion_rng'] = [.0, .025]
+        self.lft.jnts[1]['loc_motionax'] = np.array([0, -1, 0])
+        self.lft.lnks[1]['name'] = "finger1"
+        self.lft.lnks[1]['mesh_file'] = cm.CollisionModel(
+            os.path.join(this_dir, "meshes", "finger_v2.stl"), cdprimit_type="user_defined",
+            userdefined_cdprimitive_fn=self._finger_cdnp, expand_radius=.001)
+        self.lft.lnks[1]['rgba'] = [.35, .35, .35, 1]
+        # rgt finger
+        self.rgt = jl.JLChain(pos=cpl_end_pos, rotmat=cpl_end_rotmat, homeconf=np.zeros(1), name='rgt_finger')
+        self.rgt.jnts[1]['loc_pos'] = np.array([-0.01492498, -0.005, .05])
+        # self.rgt.jnts[1]['loc_rotmat'] = rm.rotmat_from_euler(0, 0, math.pi / 2)
+        self.rgt.jnts[1]['type'] = 'prismatic'
+        self.rgt.jnts[1]['loc_motionax'] = np.array([0, -1, 0])
+        self.rgt.lnks[1]['name'] = "finger2"
+        self.rgt.lnks[1]['mesh_file'] = cm.CollisionModel(
+            os.path.join(this_dir, "meshes", "finger_v2.stl"), cdprimit_type="user_defined",
+            userdefined_cdprimitive_fn=self._finger_cdnp, expand_radius=.001)
+        self.rgt.lnks[1]['rgba'] = [.35, .35, .35, 1]
+        # # reinitialize
+        self.body.reinitialize(cdmesh_type=cdmesh_type)
+        self.lft.reinitialize(cdmesh_type=cdmesh_type)
+        self.rgt.reinitialize(cdmesh_type=cdmesh_type)
+        # jaw width
+        self.jawwidth_rng = [0.0, .04]
+        # jaw center
+        self.jaw_center_pos = np.array([0, 0, .175])
+        # collision detection
+        self.all_cdelements = []
+        # self.enable_cc(toggle_cdprimit=enable_cc)
+
+    @staticmethod
+    def _finger_cdnp(name, radius):
+        collision_node = CollisionNode(name)
+        collision_primitive_c0 = CollisionBox(Point3(-.0035, 0.004, .025 + .003),
+                                              x=.0035 + radius, y=0.0032 + radius, z=.025 + .003 + radius)
+        collision_node.addSolid(collision_primitive_c0)
+        collision_primitive_c1 = CollisionBox(Point3(.008, 0.028 - .002, -.011),
+                                              x=.018 + radius, y=0.008 + radius, z=.011 + radius)
+        collision_node.addSolid(collision_primitive_c1)
+        collision_primitive_c2 = CollisionBox(Point3(-.005, 0.012 - .002, -.002 + .0025),
+                                              x=.005 + radius, y=0.008 + radius, z=.002 + .0025 + radius)
+        collision_node.addSolid(collision_primitive_c2)
+        return collision_node
+
+    @staticmethod
+    def _hnd_base_cdnp(name, radius):
+        collision_node = CollisionNode(name)
+        collision_primitive_c0 = CollisionBox(Point3(0, 0, .031),
+                                              x=.036 + radius, y=0.038 + radius, z=.031 + radius)
+        collision_node.addSolid(collision_primitive_c0)  # 0.62
+        collision_primitive_c1 = CollisionBox(Point3(0, 0, .067),
+                                              x=.036 + radius, y=0.027 + radius, z=.003 + radius)
+        collision_node.addSolid(collision_primitive_c1)  # 0.06700000
+        #
+        collision_primitive_c2 = CollisionBox(Point3(.006, .049, .0485),
+                                              x=.02 + radius, y=.02 + radius, z=.015 + radius)
+        collision_node.addSolid(collision_primitive_c2)
+        collision_primitive_c3 = CollisionBox(Point3(0, 0, .08),
+                                              x=.013 + radius, y=0.013 + radius, z=.005 + radius)
+        collision_node.addSolid(collision_primitive_c3)
+
+        return collision_node
+
+    def enable_cc(self, toggle_cdprimit):
+        if toggle_cdprimit:
+            super().enable_cc()
+            # cdprimit
+            self.cc.add_cdlnks(self.body, [0, 1])
+            self.cc.add_cdlnks(self.lft, [1])
+            self.cc.add_cdlnks(self.rgt, [1])
+            activelist = [self.body.lnks[0],
+                          self.body.lnks[1],
+                          self.lft.lnks[1],
+                          self.rgt.lnks[1]]
+            self.cc.set_active_cdlnks(activelist)
+            self.all_cdelements = self.cc.all_cdelements
+        else:
+            self.all_cdelements = [self.body.lnks[0],
+                                   self.body.lnks[1],
+                                   self.lft.lnks[1],
+                                   self.rgt.lnks[1]]
+        # cdmesh
+        for cdelement in self.all_cdelements:
+            cdmesh = cdelement['collision_model'].copy()
+            self.cdmesh_collection.add_cm(cdmesh)
+
+    def fix_to(self, pos, rotmat):
+        self.pos = pos
+        self.rotmat = rotmat
+        self.coupling.fix_to(self.pos, self.rotmat)
+        cpl_end_pos = self.coupling.jnts[-1]['gl_posq']
+        cpl_end_rotmat = self.coupling.jnts[-1]['gl_rotmatq']
+        self.body.fix_to(cpl_end_pos, cpl_end_rotmat)
+        self.lft.fix_to(cpl_end_pos, cpl_end_rotmat)
+        self.rgt.fix_to(cpl_end_pos, cpl_end_rotmat)
+
+    def fk(self, motion_val):
+        """
+        lft_outer is the only active joint, all others mimic this one
+        :param: motion_val, meter or radian
+        """
+        if self.lft.jnts[1]['motion_rng'][0] <= -motion_val <= self.lft.jnts[1]['motion_rng'][1]:
+            self.lft.jnts[1]['motion_val'] = motion_val
+            self.rgt.jnts[1]['motion_val'] = self.lft.jnts[1]['motion_val']
+            self.lft.fk()
+            self.rgt.fk()
+        else:
+            raise ValueError("The motion_val parameter is out of range!")
+
+    def jaw_to(self, jaw_width):
+        if jaw_width > self.jawwidth_rng[1]:
+            raise ValueError("The jaw_width parameter is out of range!")
+        self.fk(motion_val=-jaw_width / 2.0)
+
+    def get_jawwidth(self):
+        return -self.lft.jnts[1]['motion_val'] * 2
+
+    def gen_stickmodel(self,
+                       tcp_jnt_id=None,
+                       tcp_loc_pos=None,
+                       tcp_loc_rotmat=None,
+                       toggle_tcpcs=False,
+                       toggle_jntscs=False,
+                       toggle_connjnt=False,
+                       name='lite6wrs_gripper_stickmodel'):
+        stickmodel = mc.ModelCollection(name=name)
+        self.coupling.gen_stickmodel(tcp_loc_pos=None,
+                                     tcp_loc_rotmat=None,
+                                     toggle_tcpcs=False,
+                                     toggle_jntscs=toggle_jntscs).attach_to(stickmodel)
+        self.body.gen_stickmodel(tcp_jnt_id=tcp_jnt_id,
+                                 tcp_loc_pos=tcp_loc_pos,
+                                 tcp_loc_rotmat=tcp_loc_rotmat,
+                                 toggle_tcpcs=False,
+                                 toggle_jntscs=toggle_jntscs,
+                                 toggle_connjnt=toggle_connjnt).attach_to(stickmodel)
+        self.lft.gen_stickmodel(tcp_jnt_id=tcp_jnt_id,
+                                tcp_loc_pos=tcp_loc_pos,
+                                tcp_loc_rotmat=tcp_loc_rotmat,
+                                toggle_tcpcs=False,
+                                toggle_jntscs=toggle_jntscs,
+                                toggle_connjnt=toggle_connjnt).attach_to(stickmodel)
+        self.rgt.gen_stickmodel(tcp_loc_pos=None,
+                                tcp_loc_rotmat=None,
+                                toggle_tcpcs=False,
+                                toggle_jntscs=toggle_jntscs,
+                                toggle_connjnt=toggle_connjnt).attach_to(stickmodel)
+        if toggle_tcpcs:
+            jaw_center_gl_pos = self.rotmat.dot(self.jaw_center_pos) + self.pos
+            jaw_center_gl_rotmat = self.rotmat.dot(self.jaw_center_rotmat)
+            gm.gen_dashstick(spos=self.pos,
+                             epos=jaw_center_gl_pos,
+                             thickness=.0062,
+                             rgba=[.5, 0, 1, 1],
+                             type="round").attach_to(stickmodel)
+            gm.gen_mycframe(pos=jaw_center_gl_pos, rotmat=jaw_center_gl_rotmat).attach_to(stickmodel)
+
+        return stickmodel
+
+    def gen_meshmodel(self,
+                      tcp_jnt_id=None,
+                      tcp_loc_pos=None,
+                      tcp_loc_rotmat=None,
+                      toggle_tcpcs=False,
+                      toggle_jntscs=False,
+                      rgba=None,
+                      name='lite6wrs_gripper_meshmodel'):
+        meshmodel = mc.ModelCollection(name=name)
+        self.coupling.gen_meshmodel(tcp_loc_pos=None,
+                                    tcp_loc_rotmat=None,
+                                    toggle_tcpcs=False,
+                                    toggle_jntscs=toggle_jntscs,
+                                    rgba=rgba).attach_to(meshmodel)
+        self.body.gen_meshmodel(tcp_jnt_id=tcp_jnt_id,
+                                tcp_loc_pos=tcp_loc_pos,
+                                tcp_loc_rotmat=tcp_loc_rotmat,
+                                toggle_tcpcs=False,
+                                toggle_jntscs=toggle_jntscs,
+                                rgba=rgba).attach_to(meshmodel)
+        self.lft.gen_meshmodel(tcp_jnt_id=tcp_jnt_id,
+                               tcp_loc_pos=tcp_loc_pos,
+                               tcp_loc_rotmat=tcp_loc_rotmat,
+                               toggle_tcpcs=False,
+                               toggle_jntscs=toggle_jntscs,
+                               rgba=rgba).attach_to(meshmodel)
+        self.rgt.gen_meshmodel(tcp_loc_pos=None,
+                               tcp_loc_rotmat=None,
+                               toggle_tcpcs=False,
+                               toggle_jntscs=toggle_jntscs,
+                               rgba=rgba).attach_to(meshmodel)
+        if toggle_tcpcs:
+            jaw_center_gl_pos = self.rotmat.dot(self.jaw_center_pos) + self.pos
+            jaw_center_gl_rotmat = self.rotmat.dot(self.jaw_center_rotmat)
+            gm.gen_dashstick(spos=self.pos,
+                             epos=jaw_center_gl_pos,
+                             thickness=.0062,
+                             rgba=[.5, 0, 1, 1],
+                             type="round").attach_to(meshmodel)
+            gm.gen_mycframe(pos=jaw_center_gl_pos, rotmat=jaw_center_gl_rotmat).attach_to(meshmodel)
+        return meshmodel
+
+
+if __name__ == '__main__':
+    import visualization.panda.world as wd
+    import modeling.geometric_model as gm
+    import robot_sim.end_effectors.gripper.lite6_wrs_gripper as gp1
+
+    base = wd.World(cam_pos=[.5, .5, .5], lookat_pos=[0, 0, 0], auto_cam_rotate=False)
+    gm.gen_frame().attach_to(base)
+    # cm.CollisionModel("meshes/dual_realsense.stl", expand_radius=.001).attach_to(base)
+    grpr = Lite6WRSGripper2(enable_cc=True)
+    grpr.jaw_to(.03)
+    grpr.gen_meshmodel(toggle_tcpcs=True).attach_to(base)
+    # grpr.show_cdprimit()
+    base.run()
+
+    gp1_i = gp1.Lite6WRSGripper(pos=np.array([0, .1, 0]))
+    gp1_i.jaw_to(0.03)
+    gp1_i.gen_meshmodel().attach_to(base)
+
+    base.run()
+    grpr.jaw_to(0.03)
+
+    grpr.show_cdprimit()
+
+    gm.gen_sphere(np.array([.006, 0.049, .062]), radius=.002).attach_to(base)
+
+    base.run()
---
+++ robot_sim/robots/xarm_lite6_wrs/xarm_lite6_wrs_v2.py
@@ -0,0 +1,267 @@
+"""
+Simulation for the XArm Lite 6 With the WRS gripper
+Author: Chen Hao (chen960216@gmail.com), 20220925, osaka
+Reference: The code is implemented referring to the 'robot_sim/robots/ur3e_dual/'
+"""
+import os
+import numpy as np
+import basis.robot_math as rm
+import modeling.model_collection as mc
+import robot_sim._kinematics.jlchain as jl
+from robot_sim.manipulators.xarm_lite6 import XArmLite6
+from robot_sim.end_effectors.gripper.lite6_wrs_gripper.lite6_wrs_gripper_v2 import Lite6WRSGripper2
+import robot_sim.robots.robot_interface as ri
+
+
+class XArmLite6WRSGripper(ri.RobotInterface):
+
+    def __init__(self, pos=np.zeros(3), rotmat=np.eye(3), name='xarm_lite6', enable_cc=True):
+        super().__init__(pos=pos, rotmat=rotmat, name=name)
+        this_dir, this_filename = os.path.split(__file__)
+        # left side
+        self.body = jl.JLChain(pos=pos, rotmat=rotmat, homeconf=np.zeros(0), name='body_jl')
+        self.body.lnks[0]['name'] = "xarm_stand"
+        self.body.lnks[0]['loc_pos'] = np.array([0, 0, 0])
+        self.body.lnks[0]['rgba'] = [.55, .55, .55, 1.0]
+        self.body.lnks[0]['mesh_file'] = os.path.join(this_dir, "meshes", "xarm_lite6_stand.stl")
+        self.body.reinitialize()
+        self.arm = XArmLite6(pos=self.body.jnts[-1]['gl_posq'],
+                             rotmat=self.body.jnts[-1]['gl_rotmatq'],
+                             enable_cc=False)
+        arm_tcp_rotmat = self.arm.jnts[-1]['gl_rotmatq']
+        self.hnd = Lite6WRSGripper2(pos=self.arm.jnts[-1]['gl_posq'], rotmat=arm_tcp_rotmat,
+                                    enable_cc=False)
+
+        # tool center point
+        self.arm.jlc.tcp_jnt_id = -1
+        self.arm.jlc.tcp_loc_pos = self.hnd.jaw_center_pos
+        self.arm.jlc.tcp_loc_rotmat = self.hnd.jaw_center_rotmat
+        # a list of detailed information about objects in hand, see CollisionChecker.add_objinhnd
+        self.oih_infos = []
+        # collision detection
+        if enable_cc:
+            self.enable_cc()
+        # component map
+        self.manipulator_dict['arm'] = self.arm
+        self.manipulator_dict['hnd'] = self.arm  # specify which hand is a gripper installed to
+        self.hnd_dict['hnd'] = self.hnd
+        self.hnd_dict['arm'] = self.hnd
+
+    def enable_cc(self):
+        super().enable_cc()
+        # raise NotImplementedError
+        self.cc.add_cdlnks(self.body, [0])
+        self.cc.add_cdlnks(self.arm, [0, 1, 2, 3, 4, 5, 6])
+        self.cc.add_cdlnks(self.hnd.body, [0, 1])
+        self.cc.add_cdlnks(self.hnd.lft, [1])
+        self.cc.add_cdlnks(self.hnd.rgt, [1])
+        # lnks used for cd with external stationary objects
+        activelist = [self.arm.lnks[2],
+                      self.arm.lnks[3],
+                      self.arm.lnks[4],
+                      self.arm.lnks[5],
+                      self.arm.lnks[6],
+                      self.hnd.body.lnks[0],
+                      self.hnd.body.lnks[1],
+                      self.hnd.lft.lnks[1],
+                      self.hnd.rgt.lnks[1], ]
+        self.cc.set_active_cdlnks(activelist)
+        # lnks used for arm-body collision detection
+        # fromlist = [self.body.lnks[0],
+        #             self.arm.lnks[1], ]
+        fromlist = [self.body.lnks[0],
+                    self.arm.lnks[0],
+                    self.arm.lnks[1], ]
+        intolist = [self.arm.lnks[3],
+                    self.arm.lnks[4],
+                    self.arm.lnks[5],
+                    self.arm.lnks[6],
+                    self.hnd.body.lnks[0],
+                    self.hnd.body.lnks[1],
+                    self.hnd.lft.lnks[1],
+                    self.hnd.rgt.lnks[1], ]
+        self.cc.set_cdpair(fromlist, intolist)
+        # lnks used for arm-body collision detection -- extra
+        # fromlist = [self.body.lnks[0]]  # body
+        # intolist = [self.arm.lnks[2], ]
+        # self.cc.set_cdpair(fromlist, intolist)
+        # lnks used for in-arm collision detection
+        fromlist = [self.arm.lnks[2]]
+        intolist = [self.arm.lnks[4],
+                    self.arm.lnks[5],
+                    self.hnd.body.lnks[0],
+                    self.hnd.body.lnks[1],
+                    self.hnd.lft.lnks[1],
+                    self.hnd.rgt.lnks[1]]
+        self.cc.set_cdpair(fromlist, intolist)
+
+    def fix_to(self, pos, rotmat):
+        self.pos = pos
+        self.rotmat = rotmat
+        self.body.fix_to(self.pos, self.rotmat)
+        self.arm.fix_to(pos=self.body.jnts[-1]['gl_posq'], rotmat=self.body.jnts[-1]['gl_rotmatq'])
+        arm_tcp_rotmat = self.arm.jnts[-1]['gl_rotmatq']
+        self.hnd.fix_to(pos=self.arm.jnts[-1]['gl_posq'],
+                        rotmat=arm_tcp_rotmat)
+        # update objects in hand if available
+        for obj_info in self.oih_infos:
+            gl_pos, gl_rotmat = self.arm.cvt_loc_tcp_to_gl(obj_info['rel_pos'], obj_info['rel_rotmat'])
+            obj_info['gl_pos'] = gl_pos
+            obj_info['gl_rotmat'] = gl_rotmat
+
+    def fk(self, component_name, jnt_values):
+        """
+        :param jnt_values: 1x6 or 1x12 nparray
+        :hnd_name 'lft_arm', 'rgt_arm', 'both_arm'
+        :param component_name:
+        :return:
+        author: weiwei
+        date: 20201208toyonaka
+        """
+
+        def update_oih(component_name='arm'):
+            # inline function for update objects in hand
+            for obj_info in self.oih_infos:
+                gl_pos, gl_rotmat = self.cvt_loc_tcp_to_gl(component_name, obj_info['rel_pos'], obj_info['rel_rotmat'])
+                obj_info['gl_pos'] = gl_pos
+                obj_info['gl_rotmat'] = gl_rotmat
+
+        def update_component(component_name, jnt_values):
+            status = self.manipulator_dict[component_name].fk(jnt_values=jnt_values)
+            arm_tcp_rotmat = self.manipulator_dict[component_name].jnts[-1]['gl_rotmatq']
+            self.hnd_dict[component_name].fix_to(
+                pos=self.manipulator_dict[component_name].jnts[-1]['gl_posq'],
+                rotmat=arm_tcp_rotmat)
+            update_oih(component_name=component_name)
+            return status
+
+        super().fk(component_name, jnt_values)
+        # examine length
+        if component_name == 'arm':
+            if not isinstance(jnt_values, np.ndarray) or jnt_values.size != self.arm.ndof:
+                raise ValueError(f"An 1x{self.arm.ndof} npdarray must be specified to move a single arm!")
+            return update_component(component_name, jnt_values)
+        else:
+            raise ValueError("The given component name is not available!")
+
+    def rand_conf(self, component_name):
+        """
+        override robot_interface.rand_conf
+        :param component_name:
+        :return:
+        author: weiwei
+        date: 20210406
+        """
+        if component_name == 'arm':
+            return super().rand_conf(component_name)
+        else:
+            raise NotImplementedError
+
+    def hold(self, hnd_name, objcm, jaw_width=None):
+        """
+        the objcm is added as a part of the robot_s to the cd checker
+        :param hnd_name:
+        :param jaw_width:
+        :param objcm:
+        :return:
+        """
+        if hnd_name not in self.hnd_dict:
+            raise ValueError("Hand name does not exist!")
+        if jaw_width is not None:
+            self.hnd_dict[hnd_name].jaw_to(jaw_width)
+        rel_pos, rel_rotmat = self.manipulator_dict[hnd_name].cvt_gl_to_loc_tcp(objcm.get_pos(), objcm.get_rotmat())
+        into_list = [self.arm.lnks[0],
+                     self.arm.lnks[1],
+                     self.arm.lnks[2],
+                     self.arm.lnks[3],
+                     self.arm.lnks[4],
+                     self.arm.lnks[5], ]
+        self.oih_infos.append(self.cc.add_cdobj(objcm, rel_pos, rel_rotmat, into_list))
+        return rel_pos, rel_rotmat
+
+    def release(self, hnd_name, objcm, jawwidth=None):
+        """
+        the objcm is added as a part of the robot_s to the cd checker
+        :param jawwidth:
+        :param objcm:
+        :return:
+        """
+        if hnd_name not in self.hnd_dict:
+            raise ValueError("Hand name does not exist!")
+        if jawwidth is not None:
+            self.hnd_dict[hnd_name].jaw_to(jawwidth)
+        for obj_info in self.oih_infos:
+            if obj_info['collision_model'] is objcm:
+                self.cc.delete_cdobj(obj_info)
+                self.oih_infos.remove(obj_info)
+                break
+
+    def gen_stickmodel(self,
+                       tcp_jnt_id=None,
+                       tcp_loc_pos=None,
+                       tcp_loc_rotmat=None,
+                       toggle_tcpcs=False,
+                       toggle_jntscs=False,
+                       toggle_connjnt=False,
+                       name='xarm_lite6_stickmodel'):
+        stickmodel = mc.ModelCollection(name=name)
+        self.body.gen_stickmodel(tcp_loc_pos=None,
+                                 tcp_loc_rotmat=None,
+                                 toggle_tcpcs=False,
+                                 toggle_jntscs=toggle_jntscs).attach_to(stickmodel)
+        self.arm.gen_stickmodel(tcp_jnt_id=tcp_jnt_id,
+                                tcp_loc_pos=tcp_loc_pos,
+                                tcp_loc_rotmat=tcp_loc_rotmat,
+                                toggle_tcpcs=toggle_tcpcs,
+                                toggle_jntscs=toggle_jntscs,
+                                toggle_connjnt=toggle_connjnt).attach_to(stickmodel)
+        self.hnd.gen_stickmodel(toggle_tcpcs=False,
+                                toggle_jntscs=toggle_jntscs,
+                                toggle_connjnt=toggle_connjnt).attach_to(stickmodel)
+        return stickmodel
+
+    def gen_meshmodel(self,
+                      tcp_jnt_id=None,
+                      tcp_loc_pos=None,
+                      tcp_loc_rotmat=None,
+                      toggle_tcpcs=False,
+                      toggle_jntscs=False,
+                      rgba=None,
+                      name='xarm_lite6_meshmodel'):
+        mm_collection = mc.ModelCollection(name=name)
+        self.body.gen_meshmodel(tcp_loc_pos=None,
+                                tcp_loc_rotmat=None,
+                                toggle_tcpcs=False,
+                                toggle_jntscs=toggle_jntscs,
+                                rgba=rgba).attach_to(mm_collection)
+        self.arm.gen_meshmodel(tcp_jnt_id=tcp_jnt_id,
+                               tcp_loc_pos=tcp_loc_pos,
+                               tcp_loc_rotmat=tcp_loc_rotmat,
+                               toggle_tcpcs=toggle_tcpcs,
+                               toggle_jntscs=toggle_jntscs,
+                               rgba=rgba).attach_to(mm_collection)
+        self.hnd.gen_meshmodel(toggle_tcpcs=False,
+                               toggle_jntscs=toggle_jntscs,
+                               rgba=rgba).attach_to(mm_collection)
+        for obj_info in self.oih_infos:
+            objcm = obj_info['collision_model']
+            objcm.set_pos(obj_info['gl_pos'])
+            objcm.set_rotmat(obj_info['gl_rotmat'])
+            objcm.copy().attach_to(mm_collection)
+        return mm_collection
+
+
+if __name__ == '__main__':
+    import visualization.panda.world as wd
+    import modeling.geometric_model as gm
+
+    base = wd.World(cam_pos=[5, 0, 3], lookat_pos=[0, 0, 1])
+    gm.gen_frame().attach_to(base)
+    xarm = XArmLite6WRSGripper(enable_cc=True)
+    rand_conf = xarm.rand_conf(component_name='arm')
+    xarm.fk("arm", rand_conf)
+    xarm.gen_meshmodel().attach_to(base
+                                   )
+    # xarm.fk("arm", np.array([0, 0, 0, 0, 0, -np.pi / 2]))
+    print("Is self collided?", xarm.is_collided())
+    base.run()
