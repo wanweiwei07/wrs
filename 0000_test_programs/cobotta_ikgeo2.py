a4953c4e822cdbd19fec212312279cdf1c93a41f
weiwei <wanweiwei07@gmail.com>
2024-11-18 22:37:54 +0900

    ikgeo

--- 0000_test_programs/cobotta_ikgeo2.py
+++ 0000_test_programs/cobotta_ikgeo2.py
@@ -8,15 +8,16 @@ base = wd.World(cam_pos=[2, 0, 1], lookat_pos=[0, 0, .3])
 mcm.mgm.gen_frame().attach_to(base)
 arm = cbta.CobottaArm(enable_cc=True)
 
-tgt_pos = rm.vec(.25, .1, .1)
-tgt_rotmat = rm.rotmat_from_euler(0, rm.pi, 0)
+tgt_pos = rm.vec(.2, .2, .1)
+tgt_rotmat = rm.rotmat_from_euler(0, rm.pi/4, 0)
 mcm.mgm.gen_frame(pos=tgt_pos, rotmat=tgt_rotmat).attach_to(base)
 
+eq_err = []
 candidate_jnt_values = []
-for q4 in rm.np.linspace(arm.jlc.jnts[3].motion_range[0], arm.jlc.jnts[3].motion_range[1], 200):
-    p01 = arm.jlc.jnts[1].loc_pos
+for q4 in rm.np.linspace(arm.jlc.jnts[3].motion_range[0], arm.jlc.jnts[3].motion_range[1], 72):
+    _p12 = arm.jlc.jnts[1].loc_pos
     R06 = tgt_rotmat
-    p06 = tgt_pos - p01 - R06 @ rm.np.array([0, 0, arm.jlc.jnts[5].loc_pos[2]])
+    p06 = tgt_pos - _p12 - R06 @ rm.np.array([0, 0, arm.jlc.jnts[5].loc_pos[2]])
     # mcm.mgm.gen_frame(pos=p06, rotmat=R06).attach_to(base)
     R34 = rm.rotmat_from_axangle(arm.jlc.jnts[3].loc_motion_ax, q4)
     h2 = arm.jlc.jnts[1].loc_motion_ax
@@ -51,44 +52,43 @@ for q4 in rm.np.linspace(arm.jlc.jnts[3].motion_range[0], arm.jlc.jnts[3].motion
                             p2 = R06.T @ R01 @ h2
                             k = -arm.jlc.jnts[5].loc_motion_ax
                             q6, is_ls = sp1_lib.sp1_run(p1, p2, k)
-                            if sp1_lib.sp1_error(p1, p2, k, q6) < 0.073:
-                            # if not is_ls:
+                            if not is_ls:
                                 if arm.jlc.jnts[5].motion_range[0] < q6 < arm.jlc.jnts[5].motion_range[1]:
                                     R56 = rm.rotmat_from_axangle(arm.jlc.jnts[5].loc_motion_ax, q6)
-                                    e_q4 = h2.T @ R01.T @ R06 @ R56.T @ R45.T @ R34.T @ h2 - 1
-                                    if abs(e_q4) < 1e-15:
-                                        # sub-problem 1 for q2
-                                        p1 = p23
-                                        R10066554 = R01.T @ R06 @ R56.T @ R45.T
-                                        p2 = R01.T @ p06 - R10066554 @ R34.T @ p34 - R10066554 @ p45
-                                        k = arm.jlc.jnts[1].loc_motion_ax
-                                        q2, is_ls = sp1_lib.sp1_run(p1, p2, k)
-                                        if arm.jlc.jnts[1].motion_range[0] < q2 < arm.jlc.jnts[1].motion_range[1]:
-                                            print(is_ls)
-                                            if sp1_lib.sp1_error(p1, p2, k, q2) < 0.073:
-                                                R12 = rm.rotmat_from_axangle(arm.jlc.jnts[1].loc_motion_ax, q2)
-                                                # sub-problem 1 for q3
-                                                p1 = p34 + R34 @ p45
-                                                p2 = R12.T @ R01.T @ p06 - p23
-                                                k = arm.jlc.jnts[2].loc_motion_ax
-                                                q3, is_ls = sp1_lib.sp1_run(p1, p2, k)
-                                                # if arm.jlc.jnts[2].motion_range[0] < q3 < arm.jlc.jnts[2].motion_range[1]:
-                                                candidate_jnt_values.append([q1, q2, q3, q4, q5, q6])
+                                    # e_q4 = h2.T @ R01.T @ R06 @ R56.T @ R45.T @ R34.T @ h2 - 1
+                                    # sub-problem 1 for q2
+                                    p1 = p23
+                                    R10066554 = R01.T @ R06 @ R56.T @ R45.T
+                                    p2 = R01.T @ p06 - R10066554 @ R34.T @ p34 - R10066554 @ p45
+                                    k = arm.jlc.jnts[1].loc_motion_ax
+                                    q2, is_ls = sp1_lib.sp1_run(p1, p2, k)
+                                    if arm.jlc.jnts[1].motion_range[0] < q2 < arm.jlc.jnts[1].motion_range[1]:
+                                        R12 = rm.rotmat_from_axangle(arm.jlc.jnts[1].loc_motion_ax, q2)
+                                        # sub-problem 1 for q3
+                                        p1 = p34 + R34 @ p45
+                                        p2 = R12.T @ R01.T @ p06 - p23
+                                        k = arm.jlc.jnts[2].loc_motion_ax
+                                        q3, is_ls = sp1_lib.sp1_run(p1, p2, k)
+                                        if arm.jlc.jnts[2].motion_range[0] < q3 < arm.jlc.jnts[2].motion_range[1]:
+                                            R23 = rm.rotmat_from_axangle(arm.jlc.jnts[2].loc_motion_ax, q3)
+                                            candidate_jnt_values.append([q1, q2, q3, q4, q5, q6])
 
 print(len(candidate_jnt_values))
 for jnt_values in candidate_jnt_values:
-    print(jnt_values)
     arm.goto_given_conf(jnt_values=rm.vec(*jnt_values))
     arm.gen_meshmodel(alpha=.3).attach_to(base)
-base.run()
-jnt_values = rm.vec(q1, q2, q3, q4, q5, q6)
-arm.goto_given_conf(jnt_values=jnt_values)
-arm.gen_meshmodel(alpha=.3).attach_to(base)
-# base.run()
+    center = arm.gl_tcp_pos - arm.gl_tcp_rotmat @ rm.np.array([0, 0, arm.jlc.jnts[5].loc_pos[2]])
+    mcm.mgm.gen_sphere(pos=center, radius=.005).attach_to(base)
 
-# arm.jlc._ik_solver.test_success_rate()
-arm_mesh = arm.gen_meshmodel(alpha=.3)
-arm_mesh.attach_to(base)
-tmp_arm_stick = arm.gen_stickmodel(toggle_flange_frame=True)
-tmp_arm_stick.attach_to(base)
-# base.run()
+# ddik
+jnt_values = arm.ik(tgt_pos=tgt_pos, tgt_rotmat=tgt_rotmat)
+if jnt_values is not None:
+    arm.goto_given_conf(jnt_values=jnt_values)
+    # arm.jlc._ik_solver.test_success_rate()
+    arm_mesh = arm.gen_meshmodel(alpha=.3)
+    arm_mesh.attach_to(base)
+    tmp_arm_stick = arm.gen_stickmodel(toggle_flange_frame=True)
+    tmp_arm_stick.attach_to(base)
+else:
+    print("DDIK failed.")
+base.run()
