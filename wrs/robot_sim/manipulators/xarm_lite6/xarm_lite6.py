2c00d32b213748698615a67e934be99fb3990cdc
Weiwei Wan <wanweiwei07@gmail.com>
2025-11-21 17:05:46 +0900

    lite6 ik

--- wrs/robot_sim/manipulators/xarm_lite6/xarm_lite6.py
+++ wrs/robot_sim/manipulators/xarm_lite6/xarm_lite6.py
@@ -8,6 +8,13 @@ import wrs.modeling.collision_model as mcm
 import wrs.modeling.geometric_model as mgm
 
 
+def wrap_to_range(value, jnt_range):
+    low, high = jnt_range
+    while value < low:  value += 2 * math.pi
+    while value > high: value -= 2 * math.pi
+    return value
+
+
 class XArmLite6(mi.ManipulatorInterface):
     """
     Definition for XArm Lite 6
@@ -84,20 +91,20 @@ class XArmLite6(mi.ManipulatorInterface):
     # self-defined collison model for the base link
     @staticmethod
     def _base_cdprim(name="auto", ex_radius=None):
-        pdcnd = CollisionNode(name+"_cnode")
+        pdcnd = CollisionNode(name + "_cnode")
         collision_primitive_c0 = CollisionBox(Point3(-0.008, 0, 0.0375),
                                               x=.07 + ex_radius, y=.065 + ex_radius, z=0.0375 + ex_radius)
         pdcnd.addSolid(collision_primitive_c0)
         collision_primitive_c1 = CollisionBox(Point3(0, 0, .124),
                                               x=.043 + ex_radius, y=.043 + ex_radius, z=.049 + ex_radius)
         pdcnd.addSolid(collision_primitive_c1)
-        cdprim = NodePath(name+"_cdprim")
+        cdprim = NodePath(name + "_cdprim")
         cdprim.attachNewNode(pdcnd)
         return cdprim
 
     @staticmethod
     def _link2_cdprim(name="auto", ex_radius=None):
-        pdcnd = CollisionNode(name+"_cnode")
+        pdcnd = CollisionNode(name + "_cnode")
         collision_primitive_c0 = CollisionBox(Point3(0, 0, 0.1065),
                                               x=.041 + ex_radius, y=.042 + ex_radius, z=0.0315 + ex_radius)
         pdcnd.addSolid(collision_primitive_c0)
@@ -107,20 +114,20 @@ class XArmLite6(mi.ManipulatorInterface):
         collision_primitive_c2 = CollisionBox(Point3(.2, 0, 0.0915),
                                               x=.041 + ex_radius, y=.042 + ex_radius, z=0.0465 + ex_radius)
         pdcnd.addSolid(collision_primitive_c2)
-        cdprim = NodePath(name+"_cdprim")
+        cdprim = NodePath(name + "_cdprim")
         cdprim.attachNewNode(pdcnd)
         return cdprim
 
     @staticmethod
     def _link4_cdprim(name="auto", ex_radius=None):
-        pdcnd = CollisionNode(name+"_cnode")
+        pdcnd = CollisionNode(name + "_cnode")
         collision_primitive_c0 = CollisionBox(Point3(0, 0, -0.124009),
                                               x=.041 + ex_radius, y=.042 + ex_radius, z=0.0682075 + ex_radius)
         pdcnd.addSolid(collision_primitive_c0)
         collision_primitive_c1 = CollisionBox(Point3(0, -0.063315, -0.0503),
                                               x=.041 + ex_radius, y=.021315 + ex_radius, z=.087825 + ex_radius)
         pdcnd.addSolid(collision_primitive_c1)
-        cdprim = NodePath(name+"_cdprim")
+        cdprim = NodePath(name + "_cdprim")
         cdprim.attachNewNode(pdcnd)
         return cdprim
 
@@ -148,177 +155,126 @@ class XArmLite6(mi.ManipulatorInterface):
         :param seed_jnt_values:
         :return:
         """
+        solutions = []
         tcp_loc_pos = self.loc_tcp_pos
         tcp_loc_rotmat = self.loc_tcp_rotmat
         tgt_flange_rotmat = tgt_rotmat @ tcp_loc_rotmat.T
         tgt_flange_pos = tgt_pos - tgt_flange_rotmat @ tcp_loc_pos
         rrr_pos = tgt_flange_pos - tgt_flange_rotmat[:, 2] * np.linalg.norm(self.jlc.jnts[5].loc_pos)
         rrr_x, rrr_y, rrr_z = ((rrr_pos - self.pos) @ self.rotmat).tolist()  # in base coordinate system
-        j0_value = np.pi / 2 - math.atan2(rrr_x, rrr_y)
-        if not self._is_jnt_in_range(jnt_id=0, jnt_value=j0_value):
-            return None
-        # assume a, b, c are the axis_length of shoulders and bottom of the big triangle formed by the robot arm
-        c = math.sqrt(rrr_x ** 2 + rrr_y ** 2 + (rrr_z - self.jlc.jnts[0].loc_pos[2]) ** 2)
-        a = self.jlc.jnts[2].loc_pos[0]
-        b = np.linalg.norm(self.jlc.jnts[3].loc_pos)
-        tmp_acos_target = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
-        if tmp_acos_target > 1 or tmp_acos_target < -1:
-            print("Analytical IK Failure: The triangle formed by the robot arm is violated!")
-            return None
-        j2_value = math.acos(tmp_acos_target)
-        j2_initial_offset = math.atan(abs(self.jlc.jnts[3].loc_pos[0] / self.jlc.jnts[3].loc_pos[1]))
-        j2_value = j2_value - j2_initial_offset
-        if not self._is_jnt_in_range(jnt_id=2, jnt_value=j2_value):
-            # ignore reversed elbow
-            # j2_value = math.acos(tmp_acos_target) - math.pi
-            # if not self._is_jnt_in_range(jnt_id=2, jnt_value=j2_value):
-            return None
-        tmp_acos_target = (a ** 2 + c ** 2 - b ** 2) / (2 * a * c)
-        if tmp_acos_target > 1 or tmp_acos_target < -1:
-            print("Analytical IK Failure: The triangle formed by the robot arm is violated!")
-            return None
-        j1_value_upper = math.acos(tmp_acos_target)
-        # assume d, c, e are the edges of the lower triangle formed with the ground
-        d = self.jlc.jnts[0].loc_pos[2]
-        e = math.sqrt(rrr_x ** 2 + rrr_y ** 2 + rrr_z ** 2)
-        tmp_acos_target = (d ** 2 + c ** 2 - e ** 2) / (2 * d * c)
-        if tmp_acos_target > 1 or tmp_acos_target < -1:
-            print("Analytical IK Failure: The triangle formed with the ground is violated!")
-            return None
-        j1_value_lower = math.acos(tmp_acos_target)
-        j1_value = math.pi - (j1_value_lower + j1_value_upper)
-        if not self._is_jnt_in_range(jnt_id=1, jnt_value=j1_value):
-            return None
-        # RRR
-        anchor_gl_rotmatq = self.rotmat
-        j0_gl_rotmat0 = anchor_gl_rotmatq @ self.jlc.jnts[0].loc_rotmat
-        j0_gl_rotmatq = j0_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[0].loc_motion_ax, j0_value)
-        j1_gl_rotmat0 = j0_gl_rotmatq @ self.jlc.jnts[1].loc_rotmat
-        j1_gl_rotmatq = j1_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[1].loc_motion_ax, j1_value)
-        j2_gl_rotmat0 = j1_gl_rotmatq @ self.jlc.jnts[2].loc_rotmat
-        j2_gl_rotmatq = j2_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[2].loc_motion_ax, j2_value)
-        rrr_g_rotmat = (j2_gl_rotmatq @ self.jlc.jnts[3].loc_rotmat @
-                        self.jlc.jnts[4].loc_rotmat @ self.jlc.jnts[5].loc_rotmat)
-        j3_value, j4_value, j5_value = rm.rotmat_to_euler(rrr_g_rotmat.T @ tgt_flange_rotmat, order='rzyz').tolist()
-        j4_value = -j4_value
-        # print(j3_value, j4_value, j5_value)
-        # if not (self._is_jnt_in_range(jnt_id=3, jnt_value=j3_value) and
-        #         self._is_jnt_in_range(jnt_id=4, jnt_value=j4_value) and
-        #         self._is_jnt_in_range(jnt_id=5, jnt_value=j5_value)):
-        #     return None
-        return np.array([j0_value, j1_value, j2_value, j3_value, j4_value, j5_value])
-        # # lengths of link
-        # d1, d4, d6 = .2433, .2276, .0615
-        # a3, a4 = .2, .087
-        # # global position of joint 1
-        # pos_1 = np.array([0, 0, d1])
-        # # Joint 1
-        # pos_w = flange_pos - d6 * flange_rotmat[:, 2]
-        # theta1 = np.arctan2(pos_w[1], pos_w[0])
-        # # Joint 3
-        # d1w = np.sum((pos_w - pos_1) ** 2)
-        # num3_1 = 2 * a3 * a4
-        # num3_2 = abs(- a3 ** 4 + 2 * a3 ** 2 * a4 ** 2 + 2 * a3 ** 2 * d1w + 2 * a3 ** 2 * d4 ** 2 - a4 ** 4 +
-        #              2 * a4 ** 2 * d1w - 2 * a4 ** 2 * d4 ** 2 - d1w ** 2 + 2 * d1w * d4 ** 2 - d4 ** 4)
-        # den3 = a3 ** 2 + 2 * a3 * d4 + a4 ** 2 - d1w + d4 ** 2
-        # theta3_list = [-2 * np.arctan2((num3_1 - np.sqrt(num3_2)), den3),
-        #                -2 * np.arctan2((num3_1 + np.sqrt(num3_2)), den3)]
-        # theta2_4_5_6_list = []
-        # for theta3 in theta3_list:
-        #     # Joint 2
-        #     dxy = np.sqrt(pos_w[0] ** 2 + pos_w[1] ** 2)
-        #     z_w = pos_w[2]
-        #     num_c2 = -a3 * d1 + a3 * z_w - a4 * d1 * np.sin(theta3) + a4 * dxy * np.cos(theta3) + a4 * z_w * np.sin(
-        #         theta3) + \
-        #              d1 * d4 * np.cos(theta3) + d4 * dxy * np.sin(theta3) - d4 * z_w * np.cos(theta3)
-        #     num_s2 = a3 * dxy + a4 * d1 * np.cos(theta3) + a4 * dxy * np.sin(theta3) - a4 * z_w * np.cos(theta3) + \
-        #              d1 * d4 * np.sin(theta3) - d4 * dxy * np.cos(theta3) - d4 * z_w * np.sin(theta3)
-        #     den_cs2 = a3 ** 2 + 2 * a3 * a4 * np.sin(theta3) - 2 * a3 * d4 * np.cos(theta3) + a4 ** 2 * np.sin(
-        #         theta3) ** 2 + \
-        #               a4 ** 2 * np.cos(theta3) ** 2 + d4 ** 2 * np.sin(theta3) ** 2 + d4 ** 2 * np.cos(theta3) ** 2
-        #     if den_cs2 < 0:
-        #         num_s2 = -num_s2
-        #         num_c2 = -num_c2
-        #     theta2 = np.arctan2(num_s2, num_c2)
-        #     # Joint 4,5,6
-        #     U06 = rm.homomat_from_posrot(pos=pos_w, rotmat=tgt_rotmat)
-        #     s1, c1, s2, c2, s3, c3 = np.sin(theta1), np.cos(theta1), np.sin(theta2), np.cos(theta2), np.sin(
-        #         theta3), np.cos(theta3)
-        #     T03 = np.array([[s2 * s3 * c1 + c1 * c2 * c3, s2 * c1 * c3 - s3 * c1 * c2, s1, a3 * s2 * c1],
-        #                     [s1 * s2 * s3 + s1 * c2 * c3, s1 * s2 * c3 - s1 * s3 * c2, -c1, a3 * s1 * s2],
-        #                     [-s2 * c3 + s3 * c2, s2 * s3 + c2 * c3, 0, a3 * c2 + d1],
-        #                     [0, 0, 0, 1]])
-        #     """
-        #     U36 =
-        #     [[-sin(θ4)*sin(θ6) + cos(θ4)*cos(θ5)*cos(θ6), -sin(θ4)*cos(θ6) - sin(θ6)*cos(θ4)*cos(θ5), -sin(θ5)*cos(θ4), a4],
-        #      [-sin(θ5)*cos(θ6), sin(θ5)*sin(θ6), -cos(θ5), -d4],
-        #      [sin(θ4)*cos(θ5)*cos(θ6) + sin(θ6)*cos(θ4), -sin(θ4)*sin(θ6)*cos(θ5) + cos(θ4)*cos(θ6), -sin(θ4)*sin(θ5), 0],
-        #      [0, 0, 0, 1]]
-        #     """
-        #     U36 = (np.linalg.inv(T03)).dot(U06)
-        #     theta6 = -np.arctan2(U36[1, 1], U36[1, 0])
-        #     theta4 = np.arctan2(U36[2, 2], U36[0, 2])
-        #     c5 = -U36[1, 2]
-        #     if abs(U36[1, 0] / np.cos(theta6)) < abs(U36[1, 1] / np.sin(theta6)):
-        #         s5 = -U36[1, 0] / np.cos(theta6)
-        #     else:
-        #         s5 = U36[1, 1] / np.sin(theta6)
-        #     theta5 = np.arctan2(s5, c5)
-        #     theta2_4_5_6_list.append((theta2, theta4, theta5, theta6))
-        # # Adapt to joint range
-        # if theta3_list[0] < .0:
-        #     theta3_list[0] = 2. * np.pi + theta3_list[0]
-        # if theta3_list[1] < .0:
-        #     theta3_list[1] = 2. * np.pi + theta3_list[1]
-        # candidate_jnt_values_list = [np.array([theta1, j2, j3, j4, j5, j6]) for j3, (j2, j4, j5, j6) in
-        #                              zip(theta3_list, theta2_4_5_6_list)]
-        # jnt_values_list = []
-        # for jnt_values in candidate_jnt_values_list:
-        #     if ((self.jlc.jnt_ranges[:, 0] <= jnt_values) & (jnt_values <= self.jlc.jnt_ranges[:, 1])).all():
-        #         jnt_values_list.append(jnt_values)
-        # # if not ((self.jlc.jnts[1].motion_range[0] <= jnt_values_list[1][1] <= self.jlc.jnts[1].motion_range[1]) and
-        # #         (self.jlc.jnts[2].motion_range[0] <= jnt_values_list[1][2] <= self.jlc.jnts[2].motion_range[1]) and
-        # #         (self.jlc.jnts[4].motion_range[0] <= jnt_values_list[1][4] <= self.jlc.jnts[4].motion_range[1])):
-        # #     jnt_values_list.pop(1)
-        # # if not ((self.jlc.jnts[1].motion_range[0] <= jnt_values_list[0][1] <= self.jlc.jnts[1].motion_range[1]) and
-        # #         (self.jlc.jnts[2].motion_range[0] <= jnt_values_list[0][2] <= self.jlc.jnts[2].motion_range[1]) and
-        # #         (self.jlc.jnts[4].motion_range[0] <= jnt_values_list[0][4] <= self.jlc.jnts[4].motion_range[1])):
-        # #     jnt_values_list.pop(0)
-        # if len(jnt_values_list) == 0:
-        #     return None
-        # if option == "single" or option is None:
-        #     if len(jnt_values_list) == 1:
-        #         return jnt_values_list[0]
-        #     else:
-        #         # return joint values close to seed_jnt_values
-        #         seed_jnt_values = np.zeros(6) if seed_jnt_values is None else seed_jnt_values
-        #         if np.linalg.norm(jnt_values_list[0] - seed_jnt_values) < np.linalg.norm(
-        #                 jnt_values_list[1] - seed_jnt_values):
-        #             return jnt_values_list[0]
-        #         else:
-        #             return jnt_values_list[1]
-        # if option == "multiple":
-        #     return jnt_values_list
+
+        j0_value_candidates = [math.atan2(rrr_y, rrr_x)]
+        for j0_value in j0_value_candidates:
+            if not self._is_jnt_in_range(jnt_id=0, jnt_value=j0_value):
+                continue
+            # assume a, b, c are the axis_length of shoulders and bottom of the big triangle formed by the robot arm
+            c = math.sqrt(rrr_x ** 2 + rrr_y ** 2 + (rrr_z - self.jlc.jnts[0].loc_pos[2]) ** 2)
+            a = self.jlc.jnts[2].loc_pos[0]
+            b = np.linalg.norm(self.jlc.jnts[3].loc_pos)
+            tmp_acos_target = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
+            if tmp_acos_target > 1 or tmp_acos_target < -1:
+                print("Analytical IK Failure: The triangle formed by the robot arm is violated!")
+                continue
+            j2_value_candidates = []
+            j2_value = math.acos(tmp_acos_target)
+            j2_initial_offset = math.atan(abs(self.jlc.jnts[3].loc_pos[0] / self.jlc.jnts[3].loc_pos[1]))
+            j2_value_candidates.append(j2_value - j2_initial_offset)
+            j2_value_candidates.append(wrap_to_range(-(j2_value + j2_initial_offset), self.jnt_ranges[2]))
+            for id, j2_value in enumerate(j2_value_candidates):
+                if not self._is_jnt_in_range(jnt_id=2, jnt_value=j2_value):
+                    # ignore reversed elbow
+                    # j2_value = math.acos(tmp_acos_target) - math.pi
+                    # if not self._is_jnt_in_range(jnt_id=2, jnt_value=j2_value):
+                    continue
+                tmp_acos_target = (a ** 2 + c ** 2 - b ** 2) / (2 * a * c)
+                if tmp_acos_target > 1 or tmp_acos_target < -1:
+                    print("Analytical IK Failure: The triangle formed by the robot arm is violated!")
+                    continue
+                j1_value_upper = math.acos(tmp_acos_target)
+                # assume d, c, e are the edges of the lower triangle formed with the ground
+                d = self.jlc.jnts[0].loc_pos[2]
+                e = math.sqrt(rrr_x ** 2 + rrr_y ** 2 + rrr_z ** 2)
+                tmp_acos_target = (d ** 2 + c ** 2 - e ** 2) / (2 * d * c)
+                if tmp_acos_target > 1 or tmp_acos_target < -1:
+                    print("Analytical IK Failure: The triangle formed with the ground is violated!")
+                    continue
+                j1_value_lower = math.acos(tmp_acos_target)
+                if id == 0:
+                    j1_value = math.pi - (j1_value_lower + j1_value_upper)
+                elif id == 1:
+                    j1_value = j1_value_upper + math.pi - j1_value_lower
+                if not self._is_jnt_in_range(jnt_id=1, jnt_value=j1_value):
+                    continue
+                # RRR
+                anchor_gl_rotmatq = self.rotmat
+                j0_gl_rotmat0 = anchor_gl_rotmatq @ self.jlc.jnts[0].loc_rotmat
+                j0_gl_rotmatq = j0_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[0].loc_motion_ax, j0_value)
+                j1_gl_rotmat0 = j0_gl_rotmatq @ self.jlc.jnts[1].loc_rotmat
+                j1_gl_rotmatq = j1_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[1].loc_motion_ax, j1_value)
+                j2_gl_rotmat0 = j1_gl_rotmatq @ self.jlc.jnts[2].loc_rotmat
+                j2_gl_rotmatq = j2_gl_rotmat0 @ rm.rotmat_from_axangle(self.jlc.jnts[2].loc_motion_ax, j2_value)
+                rrr_g_rotmat = (j2_gl_rotmatq @ self.jlc.jnts[3].loc_rotmat @
+                                self.jlc.jnts[4].loc_rotmat @ self.jlc.jnts[5].loc_rotmat)
+                j3_value, j4_value, j5_value = rm.rotmat_to_euler(rrr_g_rotmat.T @ tgt_flange_rotmat,
+                                                                  order='rzyz').tolist()
+                j4_value = -j4_value
+                solutions.append(np.array([j0_value, j1_value, j2_value, j3_value, j4_value, j5_value]))
+                if self._is_jnt_in_range(jnt_id=4, jnt_value=j4_value) and \
+                        self._is_jnt_in_range(jnt_id=4, jnt_value=-j4_value):
+                    j4_value = -j4_value
+                    j3_value -= np.pi
+                    j5_value -= np.pi
+                    solutions.append(np.array([j0_value, j1_value, j2_value, j3_value, j4_value, j5_value]))
+        if option == "single":
+            if len(solutions) == 0:
+                return None
+            else:
+                return solutions[0]
+        return solutions
 
 
 if __name__ == '__main__':
     import wrs.visualization.panda.world as wd
+    from tqdm import tqdm
 
     base = wd.World(cam_pos=[2, 0, 1], lookat_pos=[0, 0, 0])
     mgm.gen_frame().attach_to(base)
     robot = XArmLite6(enable_cc=True)
-    robot.gen_stickmodel(toggle_jnt_frames=True).attach_to(base)
-    # robot.gen_meshmodel(toggle_cdprim=True).attach_to(base)
 
-    # tgt_pos = np.array([.3, .3, .3])
-    # tgt_rotmat = rm.rotmat_from_euler(0, 0, 0)
+    success = 0
+    num_trials = 10000
+    pos_err_list = []
+    rot_err_list = []
+    visualize = False
 
-    tgt_pos = np.array([0.2995316, -0.04995615, 0.1882039])
-    tgt_rotmat = np.array([[0.03785788, 0.05806798, 0.99759455],
-                           [0.01741114, 0.99812033, -0.05875933],
-                           [-0.99913144, 0.01959376, 0.03677569]])
-    mgm.gen_frame(pos=tgt_pos, rotmat=tgt_rotmat, alpha=.3).attach_to(base)
-    jnt_values = robot.ik(tgt_pos, tgt_rotmat, option="single")
-    robot.goto_given_conf(jnt_values=jnt_values)
-    robot.gen_meshmodel(rgb=rm.const.cyan, alpha=.3).attach_to(base)
-    base.run()
+    for i in tqdm(range(num_trials)):
+        jnt = robot.rand_conf()
+        # jnt = [ 1.85502727, -2.26425354,  3.87582289,  0.87367316,  1.27778191,
+        # 2.33416951]
+        pos, rot = robot.fk(jnt)
+        print("Random jnt:", repr(jnt))
+        robot.goto_given_conf(jnt)
+        if visualize:
+            mgm.gen_frame(pos=pos, rotmat=rot).attach_to(base)
+            robot.gen_meshmodel(rgb=[1, 0, 0], alpha=0.5).attach_to(base)
+        tgt_pos, tgt_rotmat = robot.fk(jnt)
+        jnt_values = robot.ik(tgt_pos, tgt_rotmat, option="multiple")
+
+        if jnt_values is not None:
+            success += 1
+            print("IK jnt values:", repr(jnt_values))
+
+            if len(jnt_values) > 1:
+                for jnt_value in jnt_values:
+                    pos, rot = robot.fk(jnt_value)
+                    pos_err, rot_err, _ = rm.diff_between_poses(tgt_pos * 1000, tgt_rotmat, pos * 1000, rot)
+                    pos_err_list.append(pos_err)
+                    rot_err_list.append(rot_err)
+                    if visualize:
+                        mgm.gen_frame(pos=pos, rotmat=rot).attach_to(base)
+                        robot.goto_given_conf(jnt_value)
+                        robot.gen_meshmodel(rgb=[0, 0, 1], alpha=0.5).attach_to(base)
+                if visualize: base.run()
+    print(f"position error (mm): mean {np.mean(pos_err_list)}, std {np.std(pos_err_list)}")
+    print(f"rotation error (deg): mean {np.mean(rot_err_list)}, std {np.std(rot_err_list)}")
+    print(f"success rate: {success} / {num_trials} * 100% = {success / num_trials * 100}%")
